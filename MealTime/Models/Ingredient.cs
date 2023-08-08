a972048e4c6bf25e31a0bb1aaa730f5ae0d43e38
Thomas Bakken <tbakken14@gmail.com>
2023-08-08 13:53:35 -0700

    Update column data types
    Co-Authored-By: ESC18 <ponderosaprintingus@gmail.com>

--- MealTime/Models/Ingredient.cs
+++ MealTime/Models/Ingredient.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
-
 namespace MealTime.Models
 {
     public class Ingredient
--- MealTime/Models/Recipe.cs
+++ MealTime/Models/Recipe.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MealTime.Models
 {
     public class Recipe
     {
         public int RecipeId { get; set; }
+        [Column(TypeName = "varchar(255)")]
         public string Name { get; set; }
         public int CookTime { get; set; }
     }
